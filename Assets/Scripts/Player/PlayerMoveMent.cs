5e0d1bcd6bab7e51ae19a2545a50d6c2d02c9d2d
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-07-24 12:00:16 -0500

    adding project

---
+++ Assets/Scripts/Player/PlayerMoveMent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMoveMent : MonoBehaviour
+{
+    public float runSpeed;
+
+    CharacterController controller;
+
+    float horizMove = 0;
+    bool jump;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizMove * Time.fixedDeltaTime, false,jump);
+        jump = false;
+    }
+
+    void PlayerInput()
+    {
+       horizMove =  Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+}
