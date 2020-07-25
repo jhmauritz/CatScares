ecb5f5a7a2b16164d0e31be54f3be94a874ea338
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-07-25 00:06:01 -0500

    adding base things into game

---
+++ Assets/Scripts/Base/Health.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float maxHealth;
+    public float currHealth;
+
+    Animator anim;
+
+    //invulnerableTimer
+    bool invulnerable;
+    public float invTimer = 0.5f;
+
+
+    public virtual void Start()
+    {
+        anim = GetComponent<Animator>();
+        currHealth = maxHealth;
+    }
+
+    public virtual void Update()
+    {
+        Debug.LogError(invTimer);
+
+        if(invulnerable)
+        {
+            invTimer -= Time.deltaTime;
+            if(invTimer <= 0)
+            {
+                invTimer = 0.5f;
+                invulnerable = false;
+            }
+        }
+    }
+
+    public virtual void TakeDamage(float damage)
+    {
+        if (!invulnerable)
+        {
+            invulnerable = true;
+            anim.SetTrigger("isDamaged");
+            currHealth -= damage;
+        }
+
+        if(currHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EnemyHealth : Health
+{
+
+    public override void Update()
+    {
+        base.Update();
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "Health: " + currHealth;
+        Debug.LogError(invTimer);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TakeDamage(5);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TakeDamage(1);
+        }
+    }
+}
