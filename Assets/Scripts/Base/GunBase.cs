ea7efec8a624d0a3247de292f31b938960425012
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-07-26 22:17:42 -0500

    trying to get mouse to manipulate correctly

--- Assets/Scripts/Base/GunBase.cs
+++ Assets/Scripts/Base/GunBase.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GunBase : MonoBehaviour
@@ -16,17 +17,25 @@ public class GunBase : MonoBehaviour
     bool isFiring;
     bool shoot;
     Animator anim;
+    private bool m_FacingRight = true;
+    CharacterController cc;
 
     public virtual void Start()
     {
         currBullet = maxBullet;
         shootTimer = shootTimerMax;
         anim = GetComponent<Animator>();
+        cc = GetComponentInParent<CharacterController>();
     }
 
     public virtual void Update()
     {
-        if(Time.timeScale > 0)
+        var pos = Camera.main.WorldToScreenPoint(transform.position);
+        var dir = Input.mousePosition - pos;
+        var angle = Mathf.Clamp(Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg, -20, 20);
+        var angleNegative = Mathf.Clamp(Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg, -20, 20);
+
+        if (Time.timeScale > 0)
         {
             if(Input.GetButton("Fire1") && currBullet >= 0 && !shoot)
             {
@@ -50,6 +59,19 @@ public class GunBase : MonoBehaviour
                 isFiring = false;
                 anim.SetBool("isShooting", false);
             }
+
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            if (cc.m_FacingRight)
+            {
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+            else if(!cc.m_FacingRight)
+            {
+                transform.rotation = Quaternion.AngleAxis(angleNegative, Vector3.forward);
+            }
+
+            
         }
     }
 
@@ -59,5 +81,13 @@ public class GunBase : MonoBehaviour
         GameObject bulletTemp = Instantiate(bulletObject, firePoint.position, firePoint.rotation);
         yield return new WaitForSeconds(3f);
         Destroy(bulletTemp);
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        transform.Rotate(0f, 180f, 0f);
     }
 }
