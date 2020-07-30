01875118b3800d91d9e3f04b4d46eede8ddff438
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-07-30 16:00:49 -0500

    working on a*

---
+++ Assets/Scripts/Enemies/AIPatrol.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AIPatrol : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public Path path;
+    public float speed = 3;
+    public float nextWaypointDistance = 3;
+    public bool reachedEndOfPath;
+    
+    private Seeker seeker;
+    private Transform player;
+    private int currWaypoint = 0;
+
+    private void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        player = GameObject.FindObjectOfType<PlayerHealth>().transform;
+        
+        //go to first part of the path
+        seeker.StartPath(transform.position, patrolPoints[1].position, OnPathComplete);
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            //Reset Waypoints
+            currWaypoint = 0;
+        }
+    }
+
+    private void Update()
+    {
+        if (path == null)
+            return;
+
+        reachedEndOfPath = false;
+        float disttowaypoint;
+    }
+}
