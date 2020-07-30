731542c089f12ae0e35b4415aca80e559879ac2a
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-07-29 20:34:31 -0500

    i just dont understand

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Lowscope.Saving;
+using Lowscope.Saving.Components;
 using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
-    public void LoadButton()
-    {
-
-    }
+    
 }
+
