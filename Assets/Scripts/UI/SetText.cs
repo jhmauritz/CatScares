492491db2303404bbe5e285ca3119cac5593063f
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-08-05 22:22:06 -0500

    adding in some small bits

---
+++ Assets/Scripts/UI/SetText.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SetText : MonoBehaviour
+{
+    public string interactText;
+
+    private TextMeshProUGUI tmp;
+
+    private void Start()
+    {
+        tmp = GetComponentInChildren<TextMeshProUGUI>();
+
+        tmp.text = "";
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerHealth>())
+        {
+            tmp.text = interactText;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerHealth>())
+        {
+            tmp.text = "";
+        }
+    }
+}
