7b5d08062a8b33dd327861db476a4f37675975d3
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-09-04 14:19:59 -0500

    working on levgel selection

---
+++ Assets/Scripts/Questing/BackButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour
+{
+    public void BackButtonFunction()
+    {
+        SetThingsActive[] clickedArray = FindObjectsOfType<SetThingsActive>();
+
+        DescriptionRef[] descArray = FindObjectsOfType<DescriptionRef>();
+
+        foreach (var item in clickedArray)
+        {
+            item.onClickedActive = false;
+            foreach (var items in descArray)
+            {
+                items.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Questing/SetThingsActive.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetThingsActive : MonoBehaviour
+{
+    public GameObject descriptionObj;
+
+    [HideInInspector]
+    public bool onClickedActive;
+    
+    public void SetActive()
+    {
+        descriptionObj.SetActive(true);
+    }
+
+    public void SetInActive()
+    {
+        if (!onClickedActive)
+        {
+            descriptionObj.SetActive(false);
+        }
+    }
+    
+    public void SetActiveClicked()
+    {
+        descriptionObj.SetActive(true);
+        onClickedActive = !onClickedActive;
+        GetComponent<Button>().enabled = false;
+        GetComponent<Button>().enabled = true;
+    }
+}
