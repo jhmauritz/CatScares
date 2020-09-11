cd272e1b1b84e41ef44e57dde700ed959d86bf86
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-09-11 12:21:27 -0500

    dialouge syustem work

---
+++ Assets/Scripts/Questing/DialougeConstructor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialougeConstructor
+{
+    private string title;
+    private string description;
+
+    public DialougeConstructor(string title, string description)
+    {
+        this.title = description;
+        this.title = title;
+    }
+}
--- Assets/Scripts/Questing/DialougeTrigger.cs
+++ Assets/Scripts/Questing/DialougeTrigger.cs
@@ -14,19 +14,56 @@ public class DialougeTrigger : MonoBehaviour
     [SerializeField] private string dialougeTitle;
     [SerializeField] private string dialougeDescription;
     [SerializeField] private Color invisibleColor;
-    private Color visibleColor;
+    private Color visibleColorPanel;
+    private Color visibleColorTitle;
+    private Color visibleColorDescription;
 
-    private void Start()
+    private void Awake()
     {
         diaPanel = GameObject.FindGameObjectWithTag("DiaPanel").GetComponent<Image>();
         diaTitle = GameObject.FindGameObjectWithTag("DiaTitle").GetComponent<TextMeshProUGUI>();
         diaDescription = GameObject.FindGameObjectWithTag("DiaDescription").GetComponent<TextMeshProUGUI>();
-
-        diaPanel.color = invisibleColor;
+        
+        visibleColorPanel = diaPanel.color;
+        visibleColorTitle = diaTitle.color;
+        visibleColorDescription = diaDescription.color;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void Start()
     {
-        diaPanel.color = visibleColor;
+        InvisibleBox();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerInputs>())
+        {
+            VisibleBox();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerInputs>())
+        {
+            InvisibleBox();
+        }
+    }
+
+    private void VisibleBox()
+    {
+        diaPanel.color = visibleColorPanel;
+        diaTitle.color = visibleColorTitle;
+        diaDescription.color = visibleColorDescription;
+
+        diaTitle.text = dialougeTitle;
+        diaDescription.text = dialougeDescription;
+    }
+
+    private void InvisibleBox()
+    {
+        diaPanel.color = invisibleColor;
+        diaTitle.color = invisibleColor;
+        diaDescription.color = invisibleColor;
     }
 }
