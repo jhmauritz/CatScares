e1d6219f03db3baedb849bdbb62c8fca56a072ae
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-09-12 21:02:54 -0500

    more aiming work

--- Assets/Scripts/UI/Crosshair.cs
+++ Assets/Scripts/UI/Crosshair.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 public class Crosshair : MonoBehaviour
 {
     private Camera cam;
-    public Transform gun;
-    public float followSharpness;
 
-    private Vector3 followOffset;
-    
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
-        followOffset = transform.position - gun.position;
     }
 
     // Update is called once per frame
@@ -26,18 +21,14 @@ public class Crosshair : MonoBehaviour
 
     private void CrossHairMoveMent()
     {
-        //constant x value
-        Vector3 targetPos = transform.position + followOffset;
-
         //follow cursor
         Vector3 mousePos = PlayerInputs.inputs.Player.MousePosition.ReadValue<Vector2>();
         mousePos.z = transform.position.z - cam.transform.position.z;
         
-        Vector3 clampedMousPos = new Vector3(transform.position.x, 
-            Mathf.Clamp(mousePos.y, 98f, 200f), 
+        Vector3 clampedMousPos = new Vector3(Mathf.Clamp((transform.position.x + 4f), 299f, 300f), 
+            Mathf.Clamp(mousePos.y, 75f, 125f), 
             mousePos.z);
-            
-        transform.position += (targetPos - transform.position) * followSharpness;
+
         transform.position = cam.ScreenToWorldPoint(clampedMousPos);
     }
 }
