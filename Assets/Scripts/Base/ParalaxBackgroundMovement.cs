074c4ef34d79c24f4115bf01ed78394fd511075a
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-09-25 14:37:07 -0500

    just adding some code things

---
+++ Assets/Scripts/Base/ParalaxBackgroundMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxBackgroundMovement : MonoBehaviour
+{
+    private Transform mainCamera;
+    private Camera camera;
+
+    [SerializeField] private float bagroundMoveSpeed;
+    [SerializeField] private float dirX;
+    [SerializeField] private float offsetByX = 13f;
+
+    void Awake()
+    {
+        camera = Camera.main;
+        mainCamera = camera.transform;
+    }
+
+    void Update()
+    {
+        MovingBackground();
+    }
+
+    private void MovingBackground()
+    {
+        float horizMove = PlayerInputs.inputs.Player.Movement.ReadValue<float>()
+        dirX = horizMove * bagroundMoveSpeed * Time.deltaTime;
+
+        transform.position = new Vector2(transform.position.x + dirX, transform.position.y);
+
+        if(transform.position.x - mainCamera.position.x < -offsetByX)
+        {
+            transform.position = new Vector2(mainCamera.position.x + offsetByX, transform.position.y);
+        }
+        else if(transform.position.x - mainCamera.position.x > offsetByX)
+        {
+            transform.position = new Vector2(mainCamera.position.x - offsetByX, transform.position.y);
+        }
+
+    }
+
+}
