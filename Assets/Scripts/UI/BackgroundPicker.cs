41683c5ad4e0b0f23e3f3d4d18729d99008b0a33
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-09-28 16:47:57 -0700

    some new changes

---
+++ Assets/Scripts/UI/BackgroundPicker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackgroundPicker : MonoBehaviour
+{
+    [SerializeField] public Sprite nightMareBG;
+    [SerializeField] public Sprite regBG;
+    private Image bg;
+
+    void Start() 
+    {
+        bg = GetComponent<Image>();
+
+        BGPicker();
+    }
+
+    private void BGPicker()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        string sceneName = scene.name;
+
+        if(sceneName.Contains("Tut"))
+        {
+            bg.sprite = nightMareBG;
+        }
+        else
+        {
+            bg.sprite = regBG;
+        }
+    }
+}
