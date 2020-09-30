e7086d4b2a66c0a2e5990746d1f4d2c11c8de57d
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-09-30 10:29:15 -0500

    background is fixed

--- Assets/Scripts/UI/BackgroundPicker.cs
+++ Assets/Scripts/UI/BackgroundPicker.cs
@@ -8,13 +8,32 @@ public class BackgroundPicker : MonoBehaviour
 {
     [SerializeField] public Sprite nightMareBG;
     [SerializeField] public Sprite regBG;
-    private Image bg;
+    private SpriteRenderer bg;
 
     void Start() 
     {
-        bg = GetComponent<Image>();
+        bg = GetComponent<SpriteRenderer>();
 
         BGPicker();
+        ResizeSpriteToScreen();
+    }
+
+    private void ResizeSpriteToScreen()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = spriteRenderer.bounds.size.x;
+        float height = spriteRenderer.bounds.size.y;
+
+        Debug.Log("width" + width);
+        Debug.Log("height" + height);
+
+        float worldScreenHeight = (float)(Camera.main.orthographicSize * 2f);
+        float worldScreenWidth = (float)(worldScreenHeight / Screen.height * Screen.width);
+
+        transform.localScale = new Vector2(worldScreenWidth/width, worldScreenHeight/height);
     }
 
     private void BGPicker()
@@ -22,7 +41,7 @@ public class BackgroundPicker : MonoBehaviour
         Scene scene = SceneManager.GetActiveScene();
         string sceneName = scene.name;
 
-        if(sceneName.Contains("Tut"))
+        if(sceneName.Contains("Fear"))
         {
             bg.sprite = nightMareBG;
         }
