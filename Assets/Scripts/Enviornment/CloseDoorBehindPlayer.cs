292a9ce9bf76ad036949ddcbaa8e23ae50b7814b
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-10-13 13:39:41 -0500

    added shooting tutorial in

---
+++ Assets/Scripts/Enviornment/CloseDoorBehindPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDoorBehindPlayer : MonoBehaviour
+{
+    GameObject doorTrap;
+
+    public List<GameObject> targets;
+
+    void Start()
+    {
+        doorTrap = GameObject.FindGameObjectWithTag("DoorTrap");
+        doorTrap.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(targets.Count == 0)
+        {
+            doorTrap.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<PlayerHealth>())
+        {
+            doorTrap.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Enviornment/TargetsRemove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetsRemove : MonoBehaviour
+{
+    CloseDoorBehindPlayer closeDoorBehindPlayer;
+
+    void Start()
+    {
+        closeDoorBehindPlayer = FindObjectOfType<CloseDoorBehindPlayer>();
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<BasicBullet>())
+        {
+            closeDoorBehindPlayer.targets.Remove(this.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
