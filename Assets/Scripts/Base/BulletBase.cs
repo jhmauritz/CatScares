9da33d89fcc28aa5edbb4c76a404e4235b2e9d0c
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-10-13 19:36:58 -0500

    small tutfear changes

--- Assets/Scripts/Base/BulletBase.cs
+++ Assets/Scripts/Base/BulletBase.cs
@@ -59,6 +59,11 @@ public class BulletBase : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+
+        if(!collision.gameObject.GetComponent<PlayerHealth>())
+        {
+            Destroy(gameObject);
+        }
     }
 }
 
